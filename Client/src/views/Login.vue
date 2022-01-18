0ad9bc72a093b7772d796a6b62b36eb4dd652370
Andy Savela <andy.savela@gmail.com>
2022-01-18 10:46:34 -0500

    login update

--- Client/src/views/Login.vue
+++ Client/src/views/Login.vue
@@ -1,60 +1,65 @@
 <script lang="ts">
-import {computed, defineAsyncComponent, defineComponent, onMounted, reactive, ref} from "vue";
+import {
+  computed,
+  defineAsyncComponent,
+  defineComponent,
+  onMounted,
+  reactive,
+  ref,
+} from "vue";
 import AuthService from "@/services/Auth/AuthService";
-import {key, useStore} from "@/store";
-import {LoginUserDto} from "@/types/models/DTO/AuthDto";
+import { key, useStore } from "@/store";
+import { LoginUserDto } from "@/types/models/DTO/AuthDto";
 import axios from "axios";
 // import img from '@/assets/images/Login-BG.jpg'
 
 export default defineComponent({
   name: "Login",
   components: {
-    BaseInput: defineAsyncComponent(() => import("@/components/forms/BaseInput.vue"))
+    BaseInput: defineAsyncComponent(
+      () => import("@/components/forms/BaseInput.vue")
+    ),
   },
   setup(props: any) {
-    const store = useStore()
-    const currentUser = computed(() => store.state.auth.CurrentUser)
+    const store = useStore();
+    const currentUser = computed(() => store.state.auth.CurrentUser);
 
     // const bgImage = { backgroundImage: "url(" + img + ")" }
-    const bgImage = {backgroundImage: "url(src/assets/images/Login-BG.jpg)"}
+    const bgImage = { backgroundImage: "url(src/assets/images/Login-BG.jpg)" };
     let loginForm: LoginUserDto = reactive({
-      username: '',
-      password: '',
-    })
+      username: "",
+      password: "",
+    });
 
     const Login = async () => {
       try {
-
-        const res = await AuthService.loginUser(loginForm)
+        const res = await AuthService.loginUser(loginForm);
         if (res.status === 200) {
-        store.dispatch('auth/loginUser', res.data).then()
+          store.dispatch("auth/loginUser", res.data).then();
         }
-        console.log(res)
-
+        console.log(res);
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
+    };
 
-    let message = ref('')
+    let message = ref("");
     const testData = async () => {
       try {
-        const res = await axios.get("/api/test/get")
-        console.log(`RES: ${res.data.message} from Login View`)
+        const res = await axios.get("/api/test/get");
+        console.log(`RES: ${res.data.message} from Login View`);
         message.value = res.data.message;
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    }
+    };
     onMounted(() => {
-      testData()
+      testData();
+    });
 
-    })
-
-    return {loginForm, bgImage, Login, currentUser, message}
-
-  }
-})
+    return { loginForm, bgImage, Login, currentUser, message };
+  },
+});
 </script>
 
 <template>
@@ -65,18 +70,29 @@ export default defineComponent({
     </div>
     <p>here is the message: {{ message }}</p>
     <div class="section section--bottom">
-      <div class="container container--back">
-      </div>
+      <div class="container container--back"></div>
       <div class="container container--front">
         <form class="form">
           <div class="field">
-            <BaseInput v-bind="{label: 'Username', color: 'primary'}" v-model="loginForm.username"/>
+            <BaseInput
+              v-bind="{ label: 'Username', color: 'primary' }"
+              v-model="loginForm.username"
+            />
           </div>
           <div class="field">
-            <BaseInput v-bind="{label: 'Password', color: 'primary', inputType: 'password'}" v-model="loginForm.password"/>
+            <BaseInput
+              v-bind="{
+                label: 'Password',
+                color: 'primary',
+                inputType: 'password',
+              }"
+              v-model="loginForm.password"
+            />
           </div>
           <div class="field">
-            <button class="btn btn--submit" @click.prevent.stop="Login">Login</button>
+            <button class="btn btn--submit" @click.prevent.stop="Login">
+              Login
+            </button>
           </div>
         </form>
       </div>
@@ -118,6 +134,4 @@ export default defineComponent({
     }
   }
 }
-
-
 </style>
